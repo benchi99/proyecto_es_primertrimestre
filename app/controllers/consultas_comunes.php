d0808984c7f695b8794aeee5b0da23bd90731f07
Rubén Bermejo Romero <mejoruben@gmail.com>
2019-11-24 11:01:51 +0100

    wrote query

--- app/controllers/consultas_comunes.php
+++ app/controllers/consultas_comunes.php
@@ -6,8 +6,8 @@
     /**
      * Consulta la base de datos para obtener todas las tareas existentes.
      *
-     * @return array|bool|mysqli_result
-     * @throws Exception Parámetros que faltan de
+     * @return array|bool|mysqli_result Consulta
+     * @throws Exception Error al construir objetos Tarea.
      */
     function obtain_all_tasks() {
         $bd = bd_gest::get_instance();
@@ -27,6 +27,12 @@
         }
     }
 
+/**
+ * Consulta a la base de datos para obtener todos los usuarios.
+ *
+ * @return array|bool|mysqli_result Consulta
+ * @throws Exception Error al construir objetos de Usuario
+ */
     function obtain_all_users() {
         $bd = bd_gest::get_instance();
         $conexion = $bd->get_connection();
@@ -39,6 +45,38 @@
         } else {
             while ($fila = $data->fetch_assoc()) {
                 array_push($response_data, new Usuario(['id' => $fila["usr_id"]]));
+            }
+            return $response_data;
+        }
+    }
+
+/**
+ * @param $query_string
+ * @return array|bool|mysqli_result
+ * @throws Exception Error al construir objetos
+ */
+    function get_tasks($query_string) {
+        $bd = bd_gest::get_instance();
+        $conexion = $bd->get_connection();
+        $response_data = [];
+
+        $data = $conexion->query("SELECT tsk_id FROM pryt1_tarea WHERE tsk_descripcion like '%".$query_string."%' or
+                                tsk_poblacion like '%".$query_string."%' or
+                                tsk_cp like '%".$query_string."%' or
+                                tsk_provincia like '%".$query_string."%' or
+                                tsk_persona_contacto like '%".$query_string."%' or
+                                tsk_estado like '%".$query_string."%' or
+                                tsk_fecha_creacion like '%".$query_string."%' or
+                                tsk_persona_encargada like '%".$query_string."%' or
+                                tsk_fecha_realizacion like '%".$query_string."%' or
+                                tsk_anotacion_anterior like '%".$query_string."%' or
+                                tsk_anotacion_posterior like '%".$query_string."%'");
+
+        if (!$data) {
+            return $data;
+        } else {
+            while ($fila = $data->fetch_assoc()) {
+                array_push($response_data, new Tarea(['id' => $fila['tsk_id']]));
             }
             return $response_data;
         }
